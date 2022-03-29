a9ab6ddc52831ff29624ecb3c0e659e6931a843b
RamReddy98 <boppidiramreddy98@gmail.com>
2022-03-28 20:34:34 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Ram Reddy' });
+  res.render('mydata', { title: 'Ram Reddy Boppidi' });
 });
 
 module.exports = router;
